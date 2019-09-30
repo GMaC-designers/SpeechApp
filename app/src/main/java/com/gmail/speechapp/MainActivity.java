be00e86079d09880a9dac536e4e28140538f1468
GMaC Technologies <DevSharedCloud>
2019-09-30 17:54:50 +0300

    initial commit

--- app/src/main/java/com/gmail/speechapp/MainActivity.java
+++ app/src/main/java/com/gmail/speechapp/MainActivity.java
@@ -1,27 +1,35 @@
 package com.gmail.speechapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private final int REQ_CODE = 100;
-    TextView textView;
+    TextView textView, clear;
+    String outPut;
+    private TextToSpeech textToSpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.text);
+        clear = findViewById(R.id.tvClear);
+        clear.setVisibility(View.VISIBLE);
+        clear.setText("What is my name?");
         ImageView speak = findViewById(R.id.speak);
         speak.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,17 +49,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @SuppressLint("WrongConstant")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQ_CODE) {
-            if (resultCode == RESULT_OK && null ! == data){
-                ArrayList result = data
-                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                assert result != null;
-                textView.setText((CharSequence) result.get(0));
+        switch (requestCode) {
+            case REQ_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    outPut = (result != null ? result.get(0) : null);
+
+                    if (outPut != null) {
+                        if (!outPut.equals("George")) {
+                            textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                                @Override
+                                public void onInit(int status) {
+                                    if (status == TextToSpeech.SUCCESS) {
+                                        int ttsLang = textToSpeech.setLanguage(Locale.US);
+
+                                        if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                                                || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                            Log.e("TTS", "The Language is not supported!");
+                                        } else {
+                                            Log.i("TTS", "Language Supported.");
+                                        }
+                                        Log.i("TTS", "Initialization success.");
+
+
+
+                                        String Data = "That's not my name";
+                                        onSpeech(Data);
+
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                            // Toast.makeText(getApplicationContext(), "That's not my name", Toast.LENGTH_LONG).show();
+                        } else {
+                            textView.setText(outPut);
+                            String Data = "Your are correct";
+                            onSpeech(Data);
+                            clear.setText("Next");
+                        }
+                    }
+                }
+                break;
             }
-            //textView.setText(data);
         }
+    }
+
+    private void onSpeech(String Data) {
+        int speechStatus = textToSpeech.speak(Data, TextToSpeech.QUEUE_FLUSH, null);
+
+        if (speechStatus == TextToSpeech.ERROR) {
+            Log.e("TTS", "Error in converting Text to Speech!");
+        }
+    }
+
+    public void onClear(View view) {
+        textView.setText("");
+        clear.setVisibility(View.GONE);
+        Toast.makeText(getApplicationContext(), outPut, Toast.LENGTH_LONG).show();
+
     }
 }
